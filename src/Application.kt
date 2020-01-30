bf13ae2bfdd7dfa93847a6c3def7b77246fd2474
Benjamin Dupont <benjozorkfr@gmail.com>
2020-01-29 20:41:08 -0500

    Initial commit

---
+++ src/Application.kt
@@ -0,0 +1,52 @@
+package pasteng
+
+import io.ktor.application.*
+import io.ktor.request.*
+import io.ktor.routing.*
+import io.ktor.http.content.*
+import io.ktor.features.*
+import io.ktor.jackson.*
+
+import org.slf4j.event.*
+
+import java.time.*
+import java.io.File
+
+import com.fasterxml.jackson.databind.*
+
+fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
+
+@Suppress("unused") // Referenced in application.conf
+@kotlin.jvm.JvmOverloads
+fun Application.module(testing: Boolean = false) {
+
+    install(CallLogging) {
+        level = Level.INFO
+        filter { call -> call.request.path().startsWith("/") }
+    }
+
+    install(io.ktor.websocket.WebSockets) {
+        pingPeriod = Duration.ofSeconds(15)
+        timeout = Duration.ofSeconds(15)
+        maxFrameSize = Long.MAX_VALUE
+        masking = false
+    }
+
+    install(ContentNegotiation) {
+        jackson {
+            enable(SerializationFeature.INDENT_OUTPUT)
+        }
+    }
+
+    routing {
+
+        static("/") {
+            staticRootFolder = File("client/src/app/dist")
+
+            default("index.html")
+        }
+
+    }
+
+}
+
